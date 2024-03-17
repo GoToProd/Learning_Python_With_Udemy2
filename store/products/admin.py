b02d24ee1311e6765341fbb1cdb3f45396561f7e
Daniil <157521979+GoToProd@users.noreply.github.com>
2024-03-17 15:37:26 +0300

    register some admin panel
    register admin panel for products, basket, user

--- store/products/admin.py
+++ store/products/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 
-from products.models import Product, ProductCategory
+from products.models import Product, ProductCategory, Basket
 
 
-admin.site.register(Product)
+
 admin.site.register(ProductCategory)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'quantity')
+    fields = ('name', 'description', ('price', 'quantity'), 'image', 'category')
+    readonly_fields = ('description',)
+    search_fields = ('name',)
+    ordering = ('name',)
+    
+
+class BasketAdmin(admin.TabularInline):
+    model = Basket
+    readonly_fields = ('created_timestamp',)
+    fields = ('product', 'quantity', 'created_timestamp')
+    extra = 0
+    
+    class Meta:
+        verbose_name = 'корзина'
+        verbose_name_plural ='корзины'
